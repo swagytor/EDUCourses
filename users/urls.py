3e09a4013370f3fbbb2defb5fd7b45d45a1c4353
hyozan <swagytor@yandex.ru>
2023-10-05 20:35:40 +0300

    Created Simple JWT authorization

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
+from django.urls import path
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.routers import DefaultRouter
-
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from users.apps import UsersConfig
 from users.views import UserViewSet
 
@@ -9,5 +11,7 @@ router = DefaultRouter()
 router.register(r'', UserViewSet, basename='user')
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 
               ] + router.urls
--- users/views.py
+++ users/views.py
@@ -1,11 +1,12 @@
-from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ViewSet
 
 from users.models import User
 from users.serializer import UserSerializer
 
 
 # Create your views here.
-class UserViewSet(CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
+class UserViewSet(ViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
